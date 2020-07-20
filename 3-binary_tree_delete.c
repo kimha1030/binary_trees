08ab151a487303980a6599d6490bf9cf80c28a92
kimha1030 <1478@holbertonschool.com>
2020-07-20 16:56:51 -0500

    Task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - Function that deletes a binary tree
+ * @tree: pointer to the root node of the tree to delete
+ * Return: void
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
+}
