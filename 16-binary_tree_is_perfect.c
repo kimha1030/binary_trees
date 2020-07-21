bb3c2a1be91b765331280d670609edd83cfbbe5f
kimha1030 <1478@holbertonschool.com>
2020-07-21 17:50:58 -0500

    Task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,34 +27,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_full - Function that checks if a BT is full
- * @tree: pointer to the root node of the tree to traverse
- * Return: size_t
- */
-
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	size_t lh, rh;
-
-	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	lh = binary_tree_is_full(tree->left);
-	rh = binary_tree_is_full(tree->right);
-	if (lh == 0 || rh == 0)
-		return (0);
-	return (1);
-}
-
-/**
  * binary_tree_is_perfect - Function that checks if a BT is perfect
  * @tree: pointer to the root node of the tree to traverse
  * Return: size_t
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t l_height, r_height, l_full, r_full;
+	size_t l_height, r_height;
 	binary_tree_t *lef = tree->left, *rig = tree->right;
 
 	if (tree == NULL)
@@ -67,9 +46,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	r_height = binary_tree_height(tree->right);
 	if (l_height == r_height)
 	{
-		l_full = binary_tree_is_full(lef);
-		r_full = binary_tree_is_full(rig);
-		if (l_full != 0 || r_full != 0)
+		if (binary_tree_is_perfect(lef) && binary_tree_is_perfect(rig))
 			return (1);
 	}
 	return (0);
